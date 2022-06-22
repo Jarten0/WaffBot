16dc05354a6fd5bc5dd3cc84ffb83b18c14f6d2f
Jarten071 <102251609+Jarten071@users.noreply.github.com>
2022-06-21 21:53:20 -0700

    dont use any of this unless u can fix it ig
    its a huge mess that im done with for the night. dont want to deal with it anymore

--- models/twitchintergration.js
+++ models/twitchintergration.js
@@ -1,8 +1,23 @@
 //i took a bit of data from online. no need to try and figure it out when someone else already has, right?
 
-//get data from streamer theendercore
-twitch api get users -q login=theendercore
+let clinetId = "l6ioq2jiq0ky6unflw8prefxv72osf";
+let clinetSecret = "kfmxqtxlrdn13v2ydyfj9kxhy2c0ty";
 
+function getTwitchAuthorization() {
+    let url = `https://id.twitch.tv/oauth2/token?client_id=${clinetId}&client_secret=${clinetSecret}&grant_type=client_credentials`;
+
+    fetch(url, {
+    method: "POST",
+    })
+    .then((res) => res.json())
+    .then((data) => handleAuthorization(data));
+}
+
+function handleAuthorization(data) {
+    let { access_token, expires_in, token_type } = data;
+    document.write(`${token_type} ${access_token}`);
+}
+getTwitchAuthorization();
 
 
 
@@ -15,11 +30,19 @@ kfmxqtxlrdn13v2ydyfj9kxhy2c0ty
 */
 
 
-var json_file = "twitchstatus.json";
-	$.getJSON(json_file, function(t) 
-	{
-		if (t["data"].length > 0 && t["data"][0].type == 'live')
+	var json_file = "twitchstatus.json";
+		$.getJSON(json_file, function(t) 
 		{
-			console.log("Send whatever message plus stream link here.")
-		}
-	})
+			if (t["data"].length > 0 && t["data"][0].type == 'live')
+			{
+				if (live != True)
+				{
+				console.log("Send whatever message plus stream link here.")
+				var live = bool True
+				}
+			else
+			{
+
+			}
+			}
+		})
